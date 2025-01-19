55339ae73aaa7bf387facec3b0cdf93d8777511b
alextorsar <alejandro.torres.sar@gmail.com>
2025-01-19 11:50:41 +0100

    Adding automatically update of size and last_update fields in Conversation model

--- services/message.py
+++ services/message.py
@@ -14,8 +14,11 @@ def create_message(db: Session, user_message: str, conversation_id: int, user_id
     new_message = Message(created_at=created_at, user_message=user_message, answered_at=answered_at, 
                           assistant_answer=assistant_answer, conversation_id=conversation_id)
     db.add(new_message)
+    conversation.size += 1
+    conversation.last_update = answered_at
     db.commit()
     db.refresh(new_message)
+    db.refresh(conversation)
     return new_message
 
 def get_messages_from_conversation_id(db: Session, conversation_id: int, user_id: int):
